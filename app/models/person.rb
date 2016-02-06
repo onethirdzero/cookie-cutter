5fec25d13adef6326a58535d5b2cef81d75a75a0
Jordan Siaw <jordansiaw.cy@gmail.com>
2016-02-06 06:41:29 +0000

    Add black as an invalid color choice.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,8 @@ class Person < ActiveRecord::Base
   validates :weight, presence: true
   validates :height, presence: true
   validates :color, presence: true, length: {maximum: 20}
+  validate :cant_be_something
+    def cant_be_something
+      errors.add(:color, "Can't be black") if self.color.downcase == "black"
+    end
 end
